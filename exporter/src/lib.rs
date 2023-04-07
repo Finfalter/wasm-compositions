cab2fcc1d650d2365ac60ff9f359a4461b9ec1e4
Christoph Brewing <christoph.brewing@bmw.de>
2023-04-07 23:45:35 +0200

    easier function

--- exporter/src/lib.rs
+++ exporter/src/lib.rs
@@ -6,8 +6,6 @@ export_greetworld!(Greeting);
 
 impl greeting::Greeting for Greeting {
     fn greet(s: String) -> String {
-        let mut s1 = "Hello".to_string();
-        s1.push_str(&s);
-        s1
+        "Hello".to_string() + " " + &s
     }
 }
